0cf5f2cc48463ddb249c6b04c10fd14e035ec1c8
Ludovic Dev <ludovicdev.io@gmail.com>
2025-08-17 05:26:16 +0200

    Mise en ligne

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ToDoApp from './ToDoApp.tsx'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <ToDoApp />
   </StrictMode>,
 )
